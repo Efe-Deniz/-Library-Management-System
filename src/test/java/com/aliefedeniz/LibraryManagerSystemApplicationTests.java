241f636fc2a8474ae54428f8b73cfe8e07a3e3b2
Ali Efe Deniz <dnz.ali.efe@gmail.com>
2025-10-21 11:26:25 +0300

    Spring Transformation Project Started

---
+++ src/test/java/com/aliefedeniz/LibraryManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliefedeniz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
